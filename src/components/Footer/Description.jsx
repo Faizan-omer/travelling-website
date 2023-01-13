f47997ff2baaad5f7e791419b338178d63f1a3ae
Faizan-omer <omerfaizan1@gmail.com>
2023-01-13 14:29:25 +0500

    text change

--- src/components/Footer/Description.jsx
+++ src/components/Footer/Description.jsx
@@ -5,7 +5,7 @@ const Description = () => {
     <div>
       <p className="text-xl text-slate-200 font-bold mb-3 font-mono">Traveliner</p>
       <p>This is my personal project to add in my portfolio</p>
-      <p>and to demonstrate my skills in React</p>
+      <p>and to demonstrate my skills in ReactJS and web development</p>
     </div>
   );
 };
